a911488df2f64c8be434e2b052895aed6305459a
Lei Zhou 周磊 <lei.zhou@JoysonQuin.com>
2020-06-16 16:51:39 +0800

    single client

--- spring_cloud_001/cloud-provider-payment8001/src/main/java/com/ryan/springcloud/Payment8001.java
+++ spring_cloud_001/cloud-provider-payment8001/src/main/java/com/ryan/springcloud/Payment8001.java
@@ -2,9 +2,11 @@ package com.ryan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class,args);
